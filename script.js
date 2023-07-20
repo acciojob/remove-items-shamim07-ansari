ba472129d068363caabe1c5dadd26ebc219de7d9
shamim07-ansari <124763751+shamim07-ansari@users.noreply.github.com>
2023-07-20 19:32:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let select = document.getElementById("colorSelect");
+let btn =  document.querySelector("input[type=button]");
+
+btn.addEventListener("click", () => {
+
+	for(let child of select.children) {
+		if(child.value === select.value) {
+			child.remove();
+		}
+	}
+});
